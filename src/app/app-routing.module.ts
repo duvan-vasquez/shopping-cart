a4e55495498a18c4e277583a219abde99b0d2a0d
Duvan Vasquez <>
2023-07-05 15:25:10 -0500

    initial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'shopping-cart',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'shopping-cart',
+    loadChildren: () => import('./features/shopping-cart/shopping-cart.module').then( m => m.ShoppingCartPageModule)
   },
 ];
 
